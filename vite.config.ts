2f4f0c15f0843d017bc9f080d6e02f92fad9018c
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-06 20:11:13 -0300

    GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-pokefetch',
+  base: '/react-pokefetch/',
   plugins: [react()],
 })
