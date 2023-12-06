4cae95995d3cc1ee0a6fa589787a783e897a5b06
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-06 01:02:36 -0300

    Working on fetching sprites

--- src/components/fetch-container/FetchContainer.tsx
+++ src/components/fetch-container/FetchContainer.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 
 interface Pokemon {
   name: string
+  url: string
+  sprite: Blob
 }
 
 const FetchContainer: React.FC = () => {
@@ -17,12 +19,22 @@ const FetchContainer: React.FC = () => {
     fetchData()
   }, [])
 
+  const fetchSprite = async (pokemon: Pokemon) => {
+    const res = await fetch(pokemon.url)
+    const data = await res.json()
+    const resSprite = await fetch(data.sprites.front_default)
+    const sprite = resSprite.blob
+    return sprite
+  }
 
   return (
     <main className='flex flex-wrap capitalize flex-col justify-center w-full h-full items-center'>
       {
         data.map((pokemon: Pokemon) => (
-          <h2 key={pokemon.name}>{pokemon.name}</h2>
+          <div key={pokemon.name} className='text-center'>
+            {fetchSprite(pokemon)}
+            <h2 >{pokemon.name}</h2>
+          </div>
         ))
       }
     </main>
