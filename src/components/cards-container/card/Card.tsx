964bce7dbb925f7bfa574bd5faddeec6eda30ecf
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-08 17:55:57 -0300

    Card placeholders added

--- src/components/cards-container/card/Card.tsx
+++ src/components/cards-container/card/Card.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { type PokemonInt } from '../../../types'
+import styles from './Card.module.css'
 
 export const Card = ({ name, url, num }: PokemonInt): React.JSX.Element => {
   const [sprite, setSprite] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchSprite = async (): Promise<void> => {
@@ -11,6 +13,7 @@ export const Card = ({ name, url, num }: PokemonInt): React.JSX.Element => {
       const data = await res.json()
       const sprite = data.sprites.front_default
       setSprite(sprite)
+      setLoading(false)
     }
 
     void fetchSprite()
@@ -19,10 +22,16 @@ export const Card = ({ name, url, num }: PokemonInt): React.JSX.Element => {
   return (
     <Link to={`${num}`}>
       <div className={'text-center border rounded-xl m-4 cursor-pointer transition-all duration-200 hover:scale-125'}>
-        {(sprite !== null) && <img src={sprite} alt={name} />}
+      <div className={styles.imageContainer}>
+        {
+          loading
+            ? <div className={`animate-pulse ${styles.placeholder}`}/>
+            : (sprite !== null) && <img src={sprite} alt={name} />
+        }
+    </div>
         <p className='text-sm'>#{num}</p>
         <h2 className='text-sm'>{name}</h2>
-      </div >
+      </div>
     </Link>
   )
 }
