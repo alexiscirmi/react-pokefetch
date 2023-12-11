61b279825adbfc58caeb4e8a2559937323425564
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-11 03:29:41 -0300

    Minor improvements

--- src/components/selectors-container/selector/card-container/card/Card.tsx
+++ src/components/selectors-container/selector/card-container/card/Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Card.module.css'
 
 export const Card = ({ data }: any) => {
@@ -9,43 +9,20 @@ export const Card = ({ data }: any) => {
     setIsSpriteLoaded(true)
   }
 
-  const typeCheck = () => {
-    switch (data.types[0].type.name) {
-      case 'grass':
-        return '#58D68D'
-      case 'poison':
-        return '#58D68D'
-      case 'bug':
-        return '#58D68D'
-      case 'fire':
-        return '#EC7063'
-      case 'water':
-        return '#85C1E9'
-      case 'ice':
-        return '#85C1E9'
-      case 'normal':
-        return '#EAEDED'
-      case 'dragon':
-        return '#EAEDED'
-      case 'electric':
-        return '#F9E79F'
-      case 'ground':
-        return '#DC7633'
-      case 'rock':
-        return '#DC7633'
-      case 'fighting':
-        return '#DC7633'
-      case 'psychic':
-        return '#A569BD'
-      case 'ghost':
-        return '#A569BD'
-      default:
-        break;
+  const [pokemonType, setPokemonType] = useState<any>()
+
+  useEffect(() => {
+    const typeCheck = async (): Promise<void> => {
+      const res = await fetch('types.json')
+      const typesData = await res.json()
+      const type = typesData.types.find((type: any) => type.type === data.types[0].type.name)
+      setPokemonType(type)
     }
-  }
+    typeCheck()
+  }, [])
 
   return (
-    <div className={`border-yellow-300 border-solid rounded-3xl my-1 ${styles.card}`} style={{ backgroundColor: typeCheck() }}>
+    <div className={`border-yellow-300 border-solid rounded-3xl my-1 ${styles.card}`} style={pokemonType && { backgroundColor: pokemonType.color }}>
 
       {/* Header */}
       <div className={`flex text-xl justify-between mt-6 mx-6 ${styles.header}`}>
@@ -68,6 +45,16 @@ export const Card = ({ data }: any) => {
       {/* Description */}
       <div className='bg-yellow-500 h-5 my-1 text-center mx-11 rounded capitalize text-sm'>
         {(data.types[0].type.name)} Pokémon / {data.height * 10}cm. / {(data.weight / 10).toLocaleString()}kg.
+      </div>
+
+      {/* Moveset */}
+      <div className='h-15'>
+
+      </div>
+
+      {/* Weakness */}
+      <div className='text-xs'>
+        Weakness: {pokemonType && pokemonType.weakness}
       </div>
 
     </div>
