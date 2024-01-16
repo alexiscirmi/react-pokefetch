bfa3105da301451618e94e0a12c1623ba45e15a9
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2024-01-16 13:07:57 -0300

    Minor code changes

--- src/components/selectors-container/selector/card-container/card/moveset/Moveset.tsx
+++ src/components/selectors-container/selector/card-container/card/moveset/Moveset.tsx
@@ -1,38 +1,59 @@
 import { useEffect, useState } from 'react'
-import { DataInterface } from '../../../../../../types'
+import { type DataInterface } from '../../../../../../types'
 
-export const Moveset = ({ moves }: DataInterface) => {
+export const Moveset: React.FC<DataInterface> = ({ moves }) => {
   const [movesetPower, setMovesetPower] = useState([0, 0])
   const [movesetEffect, setMovesetEffect] = useState(['', ''])
 
   useEffect(() => {
-    const fetchMoveset = async () => {
+    const fetchMoveset = async (): Promise<void> => {
       const res1 = await fetch(moves[0].move.url)
       const move1 = await res1.json()
       const power1 = move1.power
-      const effect1 = move1.effect_entries[0].effect.replace('$effect_chance', move1.effect_chance)
+      const effect1 = move1.effect_entries[0].effect.replace(
+        '$effect_chance',
+        move1.effect_chance
+      )
       const res2 = await fetch(moves[1].move.url)
       const move2 = await res2.json()
       const power2 = move2.power
-      const effect2 = move2.effect_entries[0].effect.replace('$effect_chance', move2.effect_chance)
+      const effect2 = move2.effect_entries[0].effect.replace(
+        '$effect_chance',
+        move2.effect_chance
+      )
       setMovesetPower([power1, power2])
       setMovesetEffect([effect1, effect2])
     }
 
-    fetchMoveset()
+    void fetchMoveset()
   }, [])
 
   return (
     <div className='flex flex-col h-15 mx-6 my-3'>
       <div className='flex justify-between pb-1 border-b-2 border-black'>
         <div>
-          <h3 className='text-lg capitalize'>{(moves[0].move.name).replace('-', ' ')}</h3>
-          {(movesetEffect[0].length + movesetEffect[1].length) > 180
-            ? movesetEffect[0].length > 250
-              ? <p className='me-1 sm:me-2 text-justify' style={{ fontSize: '10px' }}>{movesetEffect[0].slice(0, 250) + '...'}</p>
-              : <p className='me-1 sm:me-2 text-justify' style={{ fontSize: '10px' }}>{movesetEffect[0]}</p>
-            : <p className='text-xs me-2 text-justify'>{movesetEffect[0]}</p>
-          }
+          <h3 className='text-lg capitalize'>
+            {moves[0].move.name.replace('-', ' ')}
+          </h3>
+          {movesetEffect[0].length + movesetEffect[1].length > 180 ? (
+            movesetEffect[0].length > 250 ? (
+              <p
+                className='me-1 sm:me-2 text-justify'
+                style={{ fontSize: '10px' }}
+              >
+                {movesetEffect[0].slice(0, 250) + '...'}
+              </p>
+            ) : (
+              <p
+                className='me-1 sm:me-2 text-justify'
+                style={{ fontSize: '10px' }}
+              >
+                {movesetEffect[0]}
+              </p>
+            )
+          ) : (
+            <p className='text-xs me-2 text-justify'>{movesetEffect[0]}</p>
+          )}
           {/* <p className='text-xs me-2 text-justify'>
             {movesetEffect[0].length > 120
               ? movesetEffect[0].slice(0, 120) + '...'
@@ -44,20 +65,30 @@ export const Moveset = ({ moves }: DataInterface) => {
       </div>
       <div className='flex justify-between pt-1'>
         <div>
-          <h3 className='text-lg capitalize'>{(moves[1].move.name).replace('-', ' ')}</h3>
-          {(movesetEffect[0].length + movesetEffect[1].length) > 180
-            ? movesetEffect[1].length > 300
-              ? <p className='me-1 sm:me-2 text-justify' style={{ fontSize: '10px' }}>{movesetEffect[1].slice(0, 300) + '...'}</p>
-              : <p className='me-1 sm:me-2 text-justify' style={{ fontSize: '10px' }}>{movesetEffect[1]}</p>
-            : <p className='text-xs me-2 text-justify'>{movesetEffect[1]}</p>
-          }
+          <h3 className='text-lg capitalize'>
+            {moves[1].move.name.replace('-', ' ')}
+          </h3>
+          {movesetEffect[0].length + movesetEffect[1].length > 180 ? (
+            movesetEffect[1].length > 300 ? (
+              <p
+                className='me-1 sm:me-2 text-justify'
+                style={{ fontSize: '10px' }}
+              >
+                {movesetEffect[1].slice(0, 300) + '...'}
+              </p>
+            ) : (
+              <p
+                className='me-1 sm:me-2 text-justify'
+                style={{ fontSize: '10px' }}
+              >
+                {movesetEffect[1]}
+              </p>
+            )
+          ) : (
+            <p className='text-xs me-2 text-justify'>{movesetEffect[1]}</p>
+          )}
         </div>
-        <span className='text-2xl'>
-          {movesetPower[1] !== null
-            ? movesetPower[1]
-            : 0
-          }
-        </span>
+        <span className='text-2xl'>{movesetPower[1] ?? 0}</span>
       </div>
     </div>
   )
--- src/components/selectors-container/selector/card-container/card/sprite/Sprite.tsx
+++ src/components/selectors-container/selector/card-container/card/sprite/Sprite.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import { DataInterface } from '../../../../../../types'
+import { type DataInterface } from '../../../../../../types'
 import styles from './Sprite.module.css'
 
-export const Sprite = ({ sprites, name }: DataInterface) => {
+export const Sprite: React.FC<DataInterface> = ({ sprites, name }) => {
   const [isSpriteLoaded, setIsSpriteLoaded] = useState(false)
 
-  const handleSpriteLoad = () => {
+  const handleSpriteLoad = (): void => {
     setIsSpriteLoaded(true)
   }
 
   return (
     <div className='border-yellow-400 border-solid border-4 mx-6 h-52 flex justify-center items-center backdrop-grayscale backdrop-opacity-50'>
-      {!isSpriteLoaded && <div className={`animate-pulse ${styles.placeholder}`} />}
+      {!isSpriteLoaded && (
+        <div className={`animate-pulse ${styles.placeholder}`} />
+      )}
       <img
         src={sprites.other.dream_world.front_default}
         alt={name}
